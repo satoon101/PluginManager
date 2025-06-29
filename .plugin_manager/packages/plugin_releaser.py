8c8504eb69b45702cb44be421392c9c3a14ab6ff
satoon101 <satoon101@gmail.com>
2025-06-29 15:20:13 -0400

    Implemented ui version of plugin releaser.

--- .plugin_manager/packages/plugin_releaser.py
+++ .plugin_manager/packages/plugin_releaser.py
@@ -1,36 +1,36 @@
 # ../plugin_releaser.py
 
 """Creates a release for a plugin with its current version number."""
-from contextlib import suppress
-# ==============================================================================
+# =============================================================================
 # >> IMPORTS
-# ==============================================================================
+# =============================================================================
 # Python
-from functools import cached_property
+import tkinter as tk
 from zipfile import ZIP_DEFLATED, ZipFile
 
 # Package
 from common.constants import (
+    PLUGIN_LIST,
     RELEASE_DIR,
     START_DIR,
     config,
 )
-from common.functions import clear_screen, get_plugin
+from common.interface import BaseInterface
 
 # Site-package
 from configobj import ConfigObj
 from git import Repo
 
 
-# ==============================================================================
+# =============================================================================
 # >> GLOBAL VARIABLES
-# ==============================================================================
-_version_updates = {
-    1: "MAJOR",
-    2: "MINOR",
-    3: "PATCH",
-    4: None,
-}
+# =============================================================================
+_version_updates = [
+    "MAJOR",
+    "MINOR",
+    "PATCH",
+    None,
+]
 
 # Store plugin specific directories with their respective allowed file types
 _readable_data = [
@@ -57,142 +57,184 @@ EXCEPTION_FILETYPES = {
     config["TRANSLATIONS_BASE_PATH"]: ["_server.ini"],
 }
 
-SEMANTIC_VERSIONING_COUNT = len(list(filter(None, _version_updates.values())))
 
-
-# ==============================================================================
+# =============================================================================
 # >> CLASSES
-# ==============================================================================
-class PluginReleaser:
-    version = None
+# =============================================================================
+class Interface(BaseInterface):
+
+    name = "Plugin Releaser"
+    plugin_name = None
     update_type = None
-    zip_path = None
+    new_version = None
 
-    def __init__(self, plugin_name):
-        self.plugin_name = plugin_name
-        self.plugin_repo_path = START_DIR / self.plugin_name
+    def run(self):
+        self.plugin_name = None
+        self.window.title(self.name)
+        self.clear_grid()
+        self.create_grid(data=PLUGIN_LIST)
+        self.add_back_button(self.on_back_to_main)
 
-    @cached_property
-    def plugin_repo(self):
-        return Repo(START_DIR / self.plugin_name)
+    def get_info_for_plugin(self):
+        if self.plugin_name is None:
+            return None
 
-    @cached_property
-    def info_file(self):
-        return self.plugin_repo_path.joinpath(
-            config["PLUGIN_BASE_PATH"],
-            self.plugin_name,
-            "info.ini",
-        )
-
-    @cached_property
-    def info(self):
-        return ConfigObj(self.info_file)
-
-    @cached_property
-    def semantic_version(self):
-        return [int(x) for x in self.version.split(".")]
-
-    def validate_diff(self):
-        """Validate that the plugin does not have uncommitted changes."""
-        if (
-                bool(self.plugin_repo.index.diff('HEAD')) or
-                bool(self.plugin_repo.index.diff(None)) or
-                bool(self.plugin_repo.untracked_files)
-        ):
-            print(f'Plugin "{self.plugin_name}" has uncommitted changes.')
-            return False
-
-        return True
-
-    def validate_version_exists(self):
-        """Find if we need to update the version."""
-        if not self.info_file.is_file():
-            print("No info.ini file found")
-            return False
-
-        self.version = self.info.get("version")
-        if self.version is None:
-            print('"version" not found in info.ini')
-            return False
-
-        try:
-            if len(self.semantic_version) != SEMANTIC_VERSIONING_COUNT:
-                print(f'Invalid "version" in info.ini: "{self.version}"')
-                return False
-        except ValueError:
-            print(f'Invalid "version" in info.ini: "{self.version}"')
-            return False
-
-        return True
-
-    def find_new_version(self):
-        self.update_type = self.get_version_update_type()
-        if self.update_type <= SEMANTIC_VERSIONING_COUNT:
-            self.semantic_version[self.update_type - 1] += 1
-            self.semantic_version[self.update_type:] = [0] * (
-                3 - self.update_type
+        return ConfigObj(
+            START_DIR.joinpath(
+                self.plugin_name,
+                config["PLUGIN_BASE_PATH"],
+                self.plugin_name,
+                "info.ini",
             )
-            return True
-        return False
-
-    @staticmethod
-    def get_version_update_type(previous=None):
-        """Retrieve input on which part of the version should be updated."""
-        message = ""
-        if previous is not None:
-            message += f'Invalid value given "{previous}"\n\n'
-
-        message += "Which type of version update should this be?\n\n"
-        for number, choice in sorted(_version_updates.items()):
-            message += f"\t({number}) {choice}\n"
-
-        while True:
-            clear_screen()
-            value = input(message + "\n").strip()
-            with suppress(ValueError, AssertionError):
-                value = int(value)
-                assert value in _version_updates
-                return value
-
-    def commit_update(self):
-        self.version = self.info["version"] = ".".join(
-            map(str, self.semantic_version)
         )
-        self.info.write()
-        self.plugin_repo.index.add([
-            self.info_file.replace(self.plugin_repo_path, "")[1:]
-        ])
-        self.plugin_repo.index.commit(
-            f"{_version_updates[self.update_type]} version"
-            f" update ({self.version})"
+
+    def on_click(self, option):
+        self.clear_grid()
+        self.plugin_name = option
+        self.new_version = None
+        self.update_type = None
+        repo = Repo(START_DIR / self.plugin_name)
+        if (
+            bool(repo.index.diff('HEAD')) or
+            bool(repo.index.diff(None)) or
+            bool(repo.untracked_files)
+        ):
+            label = tk.Label(
+                self.window,
+                text=(
+                    f"{option} has uncommitted changes, cannot create release"
+                ),
+                font=("times", 12, "bold"),
+            )
+            label.pack()
+
+        else:
+            current_version = self.get_info_for_plugin()["version"]
+            label_frame = tk.Frame(self.window)
+            label_frame.pack(fill="x", padx=10, pady=(10, 0))
+            label = tk.Label(
+                label_frame,
+                text=f"{option} Current Version: {current_version}",
+                font=("times", 12, "bold"),
+            )
+            label.pack()
+            button_frame = tk.Frame(self.window)
+            button_frame.pack(pady=10)
+            for i, label in enumerate(_version_updates):
+                button = tk.Button(
+                    button_frame,
+                    text=str(label),
+                    command=lambda l=label: self.on_update_type_click(l),
+                    width=15,
+                )
+                button.pack(pady=2)
+        self.add_back_button(self.run)
+
+    def on_update_type_click(self, option):
+        self.clear_grid()
+        self.update_type = option
+        current_version = self.get_info_for_plugin()["version"]
+        version_list = list(
+            map(
+                int,
+                current_version.split("."),
+            )
         )
-        self.plugin_repo.remotes.origin.push()
+        label_frame = tk.Frame(self.window)
+        label_frame.pack(fill="x", padx=10, pady=(10, 0))
+        index = _version_updates.index(option)
+        if index >= len(version_list):
+            self.create_release()
+            return
+
+        version_list[index] += 1
+        version_list[index + 1:] = [0] * (len(version_list) - (index + 1))
+        self.new_version = ".".join(map(str, version_list))
+        label = tk.Label(
+            label_frame,
+            text=(
+                f"{self.plugin_name} Current Version: {current_version}\n"
+                f"Update to version '{self.new_version}'?"
+            ),
+            font=("times", 12, "bold"),
+        )
+        label.pack()
+        button_frame = tk.Frame(self.window)
+        button_frame.pack(pady=10)
+        for label, value in (("Yes", True), ("No", False)):
+            button = tk.Button(
+                button_frame,
+                text=str(label),
+                command=lambda v=value: self.on_acknowledge_update(v),
+                width=15,
+            )
+            button.pack(pady=2)
+        self.add_back_button(lambda p=self.plugin_name: self.on_click(p))
+
+    def on_acknowledge_update(self, option):
+        if not option:
+            self.on_click(self.plugin_name)
+            return
+
+        self.create_release()
 
     def create_release(self):
-        """Verify the plugin name and create the current release."""
-        # Get the directory to save the release in
-        save_path = RELEASE_DIR / self.plugin_name
+        self.clear_grid()
+        commands = []
+        console = self.get_console()
+        repo = Repo(START_DIR / self.plugin_name)
+        if self.new_version is not None:
+            version = self.new_version
+            self.update_version(repo)
+        else:
+            version = self.get_info_for_plugin()["version"]
+        self.execute_console_commands(
+            console=console,
+            commands=commands,
+        )
+        self.add_back_button(self.run)
 
-        # Create the directory if it doesn't exist
+        save_path = RELEASE_DIR / self.plugin_name
         if not save_path.is_dir():
             save_path.makedirs()
 
-        # Get the zip file location
-        self.zip_path = save_path / f"{self.plugin_name} - v{self.version}.zip"
-
-        # Does the release already exist?
-        if self.zip_path.is_file():
+        zip_path = save_path / f"{self.plugin_name} - v{version}.zip"
+        if zip_path.is_file():
             print("Release already exists for current version.")
             return
 
-        repo_files = self.plugin_repo.git.ls_files().splitlines()
+        repo_files = repo.git.ls_files().splitlines()
 
         # Create the zip file
-        with ZipFile(self.zip_path, "w", ZIP_DEFLATED) as zip_file:
-
+        with ZipFile(zip_path, "w", ZIP_DEFLATED) as zip_file:
             for repo_file in repo_files:
                 if self.validate_file_by_base_path(repo_file):
-                    self.add_file(repo_file, zip_file)
+                    self.add_file(
+                        relative_file_path=repo_file,
+                        zip_file=zip_file,
+                        plugin_path=START_DIR / self.plugin_name,
+                    )
+
+        print(f"Saved release to {zip_path}")
+
+    def update_version(self, repo):
+        print(f"Updating {self.plugin_name} to version '{self.new_version}'")
+        info = self.get_info_for_plugin()
+        info["version"] = self.new_version
+        info.write()
+        repo.git.add(
+            "--verbose",
+            f"{config['PLUGIN_BASE_PATH']}/{self.plugin_name}/info.ini",
+        )
+        print(
+            repo.git.commit(
+                "-m",
+                f"{self.update_type} version update ({self.new_version})"
+            )
+        )
+        results = repo.remotes.origin.push()
+        for info in results:
+            print(info.summary)
 
     @staticmethod
     def validate_file_by_base_path(file):
@@ -206,51 +248,22 @@ class PluginReleaser:
                 return True
         return False
 
-    def add_file(self, relative_file_path, zip_file):
+    @staticmethod
+    def add_file(relative_file_path, zip_file, plugin_path):
         """Add the given file and all parent directories to the zip."""
-        full_file_path = self.plugin_repo_path / relative_file_path
+        full_file_path = plugin_path / relative_file_path
         zip_file.write(full_file_path, relative_file_path)
         directory = full_file_path.parent
 
         # Get all parent directories to add to the zip
-        while directory != self.plugin_repo_path:
+        while directory != plugin_path:
 
             # Is the current directory not yet included in the zip?
             current = directory.replace(
-                self.plugin_repo_path,
+                plugin_path,
                 "",
             )[1:].replace("\\", "/") + "/"
             if current not in zip_file.namelist():
                 zip_file.write(directory, current)
 
             directory = directory.parent
-
-
-# ==============================================================================
-# >> MAIN FUNCTION
-# ==============================================================================
-def run():
-    # Get the plugin to check
-    plugin_name = get_plugin(suffix="release", allow_all=False)
-
-    # Was a valid plugin chosen?
-    if plugin_name is not None:
-        clear_screen()
-
-        plugin_releaser = PluginReleaser(plugin_name)
-        if (
-            plugin_releaser.validate_diff() and
-            plugin_releaser.validate_version_exists()
-        ):
-            if plugin_releaser.find_new_version():
-                plugin_releaser.commit_update()
-            plugin_releaser.create_release()
-            print(
-                f"Successfully created {plugin_releaser.plugin_name} version"
-                f" {plugin_releaser.version} release:\n\t"
-                f'"{plugin_releaser.zip_path}"\n\n'
-            )
-
-
-if __name__ == "__main__":
-    run()
