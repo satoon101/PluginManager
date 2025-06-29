0a0b6448a1df35b6b845b80053c201b4b561108d
satoon101 <satoon101@gmail.com>
2025-06-29 15:15:40 -0400

    Split create_console_output into 2 separate methods. 1 to create the console, and 2 to execute console commands. get_console also sets stdout and stderr to output to the console, so that print and other Python code can output to the console.

--- .plugin_manager/packages/common/interface.py
+++ .plugin_manager/packages/common/interface.py
@@ -1,13 +1,31 @@
 import subprocess
+import sys
 import tkinter as tk
 
 
 BUTTONS_PER_ROW = 4
 
 
+class TextRedirector:
+    def __init__(self, text_widget, tag="stdout"):
+        self.text_widget = text_widget
+        self.tag = tag
+
+    def write(self, message):
+        self.text_widget.configure(state="normal")
+        self.text_widget.insert("end", message, (self.tag,))
+        self.text_widget.see("end")
+        self.text_widget.configure(state="disabled")
+
+    def flush(self):
+        pass  # Needed for compatibility with some streams
+
+
 class BaseInterface:
 
     name = None
+    stdout = None
+    stderr = None
 
     def __init__(self, window, main_run):
         self.window = window
@@ -18,6 +36,13 @@ class BaseInterface:
             if isinstance(widget, tk.Button) and widget.cget("text") == "Exit":
                 continue
             widget.destroy()
+        if self.stdout is None:
+            return
+
+        sys.stdout = self.stdout
+        sys.stderr = self.stderr
+        self.stdout = None
+        self.stderr = None
 
     def create_grid(self, data):
         for i, label in enumerate(data):
@@ -48,25 +73,32 @@ class BaseInterface:
         self.clear_grid()
         self.main_run()
 
-    def create_console_output(self, commands, suppress_error_msg=False):
-        process = subprocess.Popen(
-            " && ".join(commands),
-            shell=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True,
-        )
-        stdout, stderr = process.communicate()
+    def get_console(self):
         self.window.rowconfigure(0, weight=8)
         self.window.rowconfigure(1, weight=1)
         self.window.rowconfigure(2, weight=1)
         self.window.columnconfigure(0, weight=1)
-        output_text = tk.Text(self.window, wrap="word", font=("consolas", 10))
-        output_text.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
-        output_text.insert(tk.END, stdout)
-        if stderr:
-            message = stderr if suppress_error_msg else f"Errors:\n{stderr}"
-            output_text.insert(tk.END, message)
+        console = tk.Text(self.window, wrap="word", font=("consolas", 10))
+        console.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
+        console.tag_config("stdout", foreground="black")
+        console.tag_config("stderr", foreground="red")
+        self.stdout = sys.stdout
+        self.stderr = sys.stderr
+        sys.stdout = TextRedirector(console, "stdout")
+        sys.stderr = TextRedirector(console, "stderr")
+        return console
+
+    @staticmethod
+    def execute_console_commands(console, commands):
+        process = subprocess.Popen(
+            " && ".join(commands),
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            text=True,
+        )
+        for line in process.stdout:
+            console.insert("end", line)
 
     @staticmethod
     def on_click(option):
